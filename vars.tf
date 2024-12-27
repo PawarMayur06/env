e9b1650917f95294d9b65d103a02d14dd3ee021b
Mayur Kailas Pawar <mayurkailas.pawar@celebaltech.com>
2024-12-27 14:22:03 +0530

    az login

--- vars.tf
+++ vars.tf
@@ -1,11 +1,11 @@
-variable "AZURE_CLIENT_ID" { 
-}
+# variable "AZURE_CLIENT_ID" { 
+# }
 
-variable "AZURE_CLIENT_SECRET" {  
-}
+# variable "AZURE_CLIENT_SECRET" {  
+# }
 
-variable "AZURE_SUBSCRIPTION_ID" {  
-}
+# variable "AZURE_SUBSCRIPTION_ID" {  
+# }
 
-variable "AZURE_TENANT_ID" {  
-}
+# variable "AZURE_TENANT_ID" {  
+# }
