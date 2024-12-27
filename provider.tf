66c226b420fc58fcae772e288f6b7bbd774ea8a7
Mayur Kailas Pawar <mayurkailas.pawar@celebaltech.com>
2024-12-27 12:19:29 +0530

    final

--- provider.tf
+++ provider.tf
@@ -9,8 +9,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-  # subscription_id = var.subscription_id
-  # tenant_id       = var.tenant_id
-  # client_id       = var.client_id
-  # client_secret   = var.client_secret
+
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
 }
+
--- vars.tf
+++ vars.tf
@@ -1,11 +1,11 @@
-# variable "subscription_id" { 
-# }
+variable "azure_subscription_id" { 
+}
 
-# variable "tenant_id" {  
-# }
+variable "azure_tenant_id" {  
+}
 
-# variable "client_id" {  
-# }
+variable "azure_client_id" {  
+}
 
-# variable "client_secret" {  
-#}
+variable "azure_client_secret" {  
+}
